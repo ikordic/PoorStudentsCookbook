83ac013b793173f247ed5ed2df1517abcfec6871
ikordic <ivan.kordic94@gmail.com>
2019-01-17 16:01:13 +0100

    Firebase and Glide dependencies configured.

--- app/src/main/java/com/kordic/ivan/poorstudentscookbook/RecipeCardView.java
+++ app/src/main/java/com/kordic/ivan/poorstudentscookbook/RecipeCardView.java
@@ -2,15 +2,17 @@ package com.kordic.ivan.poorstudentscookbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class RecipeCardView extends AppCompatActivity {
+public class RecipeCardView extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_card_view);
 
-        //test//
-        //branch test//
+        //Firebase and Glide dependencies configured.
     }
 }
