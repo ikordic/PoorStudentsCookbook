ebaafaa8a6f7974f5f53dc074e7edefb7c22f434
ikordic <ivan.kordic94@gmail.com>
2019-01-17 14:33:44 +0100

    Initial commit

---
+++ app/src/main/java/com/kordic/ivan/poorstudentscookbook/RecipeCardView.java
@@ -0,0 +1,13 @@
+package com.kordic.ivan.poorstudentscookbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeCardView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_card_view);
+    }
+}
