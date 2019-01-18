be1e21f11ad30b207eaac3d4f26cb2a9793b1891
ikordic <ivan.kordic94@gmail.com>
2019-01-18 15:09:56 +0100

    RegisterActivity: -Register with email added -Login added -Logout added -Fetch users email added

--- app/src/main/java/com/kordic/ivan/poorstudentscookbook/RecipeCardViewActivity.java
+++ app/src/main/java/com/kordic/ivan/poorstudentscookbook/RecipeCardViewActivity.java
@@ -1,5 +1,6 @@
 package com.kordic.ivan.poorstudentscookbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -14,5 +15,7 @@ public class RecipeCardViewActivity extends AppCompatActivity
         setContentView(R.layout.activity_recipe_card_view);
 
         //Firebase and Glide dependencies configured.
+        Intent i = new Intent(RecipeCardViewActivity.this, RegisterActivity.class);
+        startActivity(i);
     }
 }
