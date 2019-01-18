a47a358615baa25d102e2b93eb00678c084f72fc
ikordic <ivan.kordic94@gmail.com>
2019-01-18 13:01:56 +0100

    Gradle update, RecipeCardView renamed, RegisterActivity created.

--- app/src/main/java/com/kordic/ivan/poorstudentscookbook/RecipeCardViewActivity.java
+++ app/src/main/java/com/kordic/ivan/poorstudentscookbook/RecipeCardViewActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class RecipeCardView extends AppCompatActivity
+public class RecipeCardViewActivity extends AppCompatActivity
 {
 
     @Override
